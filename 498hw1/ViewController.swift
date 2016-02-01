9255bd574d6425dd17964e6cc20827e82f602315
Edward Chou <Edward_Chou@wirelessprvnat-172-17-96-68.near.illinois.edu>
2016-02-01 16:39:21 -0600

    Initial Commit

---
+++ 498hw1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  498hw1
+//
+//  Created by Edward Chou on 2/1/16.
+//  Copyright © 2016 Edward Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
